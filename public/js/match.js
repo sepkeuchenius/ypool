0b23973a5424aa34d18a8422a7050c4dcf30d1e6
Sep Keuchenius <sep@y.digital>
2024-01-05 18:19:10 +0100

    merged

--- public/js/match.js
+++ public/js/match.js
@@ -1,8 +1,26 @@
-const getAllUsers = firebase.functions().httpsCallable('get_all_users');
-const saveMatch = firebase.functions().httpsCallable('save_match');
 
 function match(){
+    const getAllUsers = firebase.functions().httpsCallable('get_all_users');
+    const saveMatch = firebase.functions().httpsCallable('save_match');
     outcome = $("input[name='outcome']").val()
     opponent = $("#opponent").val()
     saveMatch({"opponent":opponent, "outcome":outcome}).then((res)=>{console.log(res)});
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const getAllUsers = firebase.functions().httpsCallable('get_all_users');
+    const saveMatch = firebase.functions().httpsCallable('save_match');
+    GetAllUsers = firebase.functions().httpsCallable('get_all_users');
+    firebase.functions().useEmulator("localhost", 5001);
+    firebase.auth().onAuthStateChanged(function (loadedUser) {
+        if (loadedUser) {
+            GetAllUsers().then(function (users) {
+                console.log(users)
+                for (user of users.data){
+                    $("#opponent").append('<option value="'+user.uid+'">'+user.name+'</option>')
+                }
+            });
+            
+        } 
+    })
+});
